f8840db072893be2744409bb50a15098924d2b6b
ant <wy361307103@163.com>
2021-10-24 16:19:20 +0800

    创建bot

--- bot.py
+++ bot.py
@@ -21,7 +21,8 @@ app = nonebot.get_asgi()
 driver = nonebot.get_driver()
 driver.register_adapter("mirai", WebsocketBot, qq=983107785)
 
-nonebot.load_builtin_plugins()
+nonebot.load_plugins("qweather")
+# nonebot.load_builtin_plugins()
 
 # Please DO NOT modify this file unless you know what you are doing!
 # As an alternative, you should use command `nb` or modify `pyproject.toml` to load plugins
