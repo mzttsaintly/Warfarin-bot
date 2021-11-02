b753f2948af7959447364990cea81ac1af0ba6b6
ant <wy361307103@163.com>
2021-11-02 18:17:17 +0800

    fix bug 新增随机获取塔罗牌功能(copy from sagiri-bot)

--- warfarin/plugins/Tarot/__init__.py
+++ warfarin/plugins/Tarot/__init__.py
@@ -3,25 +3,28 @@ import json
 import random
 from pathlib import Path
 
-from nonebot import on_keyword
+from nonebot import on_keyword, logger
 from nonebot.adapters.cqhttp import Bot, MessageEvent, MessageSegment, Message
 import nonebot
 from nonebot.rule import keyword
 
 tarot = on_keyword({"塔罗牌"}, priority=1)
 
+
 @tarot.handle()
 async def send_tarot(bot: Bot, event: MessageEvent):
     """塔罗牌"""
     card, filename = await get_random_tarot()
-    dir = random.choice(['normal', 'reverse'])
-    card_type = '正位' if dir == 'normal' else '逆位'
-    content = f"{card['name']} ({card['name-en']}) {card_type}\n牌意：{card['meaning'][dir]}"
+    image_dir = random.choice(['normal', 'reverse'])
+    card_type = '正位' if image_dir == 'normal' else '逆位'
+    content = f"{card['name']} ({card['name-en']}) {card_type}\n牌意：{card['meaning'][image_dir]}"
     elements = Message(f"{content}")
-    img_path = f"{os.getcwd()}/warfarin/plugins/Tarot/resource/{dir}/{filename + '.jpg'}"
+    img_path = os.path.join(f"{os.getcwd()}", "warfarin", "plugins", "Tarot", "resource", f"{image_dir}",
+                            f"{filename}.jpg")
+    logger.debug(f"塔罗牌图片:{img_path}")
     if filename and os.path.exists(img_path):
         elements.append(MessageSegment.image(img_path))
-    
+
     await tarot.finish(elements)
 
 
